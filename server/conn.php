e9f2127a61fd952857c3f2d0d416768632e74813
ajay panchal <ap8570091@gmail.com>
2023-01-22 23:39:51 +0530

    db

--- server/conn.php
+++ server/conn.php
@@ -1,7 +1,7 @@
 <?php
 
-// $conn =mysqli_connect("localhost","u198469606_savexa","P^6qBMDCYq","u198469606_savexa");
-$conn = mysqli_connect("localhost", "root", "", "u198469606_savexa");
+$conn =mysqli_connect("localhost","u198469606_savexa","P^6qBMDCYq","u198469606_savexa");
+// $conn = mysqli_connect("localhost", "root", "", "u198469606_savexa");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
